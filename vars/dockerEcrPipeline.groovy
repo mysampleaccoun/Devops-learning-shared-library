97f740ffcc7bf992997dedaf1e11cba9f00b72ba
mysampleaccoun <122212893+mysampleaccoun@users.noreply.github.com>
2026-08-12 09:37:29 +0530

    Update dockerEcrPipeline.groovy

--- vars/dockerEcrPipeline.groovy
+++ vars/dockerEcrPipeline.groovy
@@ -1,4 +1,51 @@
 
-def call() {
-    echo "Docker ECR pipeline template"
+def call(Map config = [:]) {
+
+    stage('Checkout') {
+        git(
+            branch: config.gitBranch,
+            url: config.gitRepo
+        )
+    }
+
+    stage('Build Docker Image') {
+        dir('jenkins') {
+            sh """
+                docker build \
+                -t ${config.imageName}:${config.imageTag} \
+                .
+            """
+        }
+    }
+
+    stage('Login to ECR') {
+        withCredentials([
+            [$class: 'AmazonWebServicesCredentialsBinding',
+             credentialsId: 'aws-ecr']
+        ]) {
+            sh """
+                aws ecr get-login-password \
+                --region ${config.awsRegion} | \
+                docker login \
+                --username AWS \
+                --password-stdin \
+                683354427061.dkr.ecr.${config.awsRegion}.amazonaws.com
+            """
+        }
+    }
+
+    stage('Tag Image') {
+        sh """
+            docker tag \
+            ${config.imageName}:${config.imageTag} \
+            683354427061.dkr.ecr.${config.awsRegion}.amazonaws.com/${config.ecrRepository}:${config.imageTag}
+        """
+    }
+
+    stage('Push to ECR') {
+        sh """
+            docker push \
+            683354427061.dkr.ecr.${config.awsRegion}.amazonaws.com/${config.ecrRepository}:${config.imageTag}
+        """
+    }
 }
